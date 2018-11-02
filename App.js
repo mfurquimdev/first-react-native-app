608a70bcafc2201bf2c23c41b981c0aa1f6b1554
Mateus Furquim <furquim.axs@gmail.com>
2018-11-02 16:45:08 -0300

    42. ComponentDidMount

--- App.js
+++ App.js
@@ -3,21 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header'
 
+import axios from 'axios';
+
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			people: []
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://randomuser.me/api?nat=br&results=5')
+			.then(response => {
+				const {results} = response.data
+				this.setState({
+					people: results
+				})
+			})
+	}
 
 	renderList() {
-		const names = [
-			'Eddie Van Halen',
-			'Jimi Hendrix',
-			'Chimbinha',
-			'Steve Vai'
-		];
 
-		const textElements = names.map((name, index) => {
-			return <Text key={index}>{name}</Text>
-		});
-
-		return textElements;
+		//return textElements;
 	}
 
 	render () {
