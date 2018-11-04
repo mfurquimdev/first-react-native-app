99ca161ea3485147e3b86bb16e1c51547335027c
Mateus Furquim <furquim.axs@gmail.com>
2018-11-04 18:39:14 -0200

    Refactoring: Adding structure for pages

--- App.js
+++ App.js
@@ -1,37 +1,3 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import { PeoplePage } from './src/pages'
 
-import Header from './src/components/Header'
-import PeopleList from './src/components/PeopleList'
-
-import axios from 'axios';
-
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			people: []
-		}
-	}
-
-	componentDidMount() {
-		axios
-			.get('https://randomuser.me/api?nat=br&results=5')
-			.then(response => {
-				const {results} = response.data
-				this.setState({
-					people: results
-				})
-			})
-	}
-
-	render () {
-		return (
-			<View>
-				<Header title="Pessoas" />
-				<PeopleList people={this.state.people} />
-			</View>
-		);
-	}
-}
+export default PeoplePage
---
+++ src/pages/PeoplePage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Header from '../components/Header'
+import PeopleList from '../components/PeopleList'
+
+import axios from 'axios';
+
+export default class PeoplePage extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			people: []
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://randomuser.me/api?nat=br&results=5')
+			.then(response => {
+				const {results} = response.data
+				this.setState({
+					people: results
+				})
+			})
+	}
+
+	render () {
+		return (
+			<View>
+				<Header title="Pessoas" />
+				<PeopleList people={this.state.people} />
+			</View>
+		);
+	}
+}
